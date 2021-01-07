7f0d5d5cb771ff7167510c9e551312d3aad689f9
Benjamin Thomas <benjaminthomas@Benjamins-MacBook-Pro.local>
2021-01-06 19:43:44 -0800

    My First Commit

---
+++ src/React/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/* Components ---------------------------*/
+//import Test from './Test.jsx';
+
+const Main = () => {
+
+    return (
+        <div className='Main'>
+        <h1>Ben's in Class</h1>
+        </div>
+    );
+}
+
+export default Main;
