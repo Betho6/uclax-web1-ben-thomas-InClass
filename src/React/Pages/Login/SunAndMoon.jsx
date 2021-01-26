342b63a6bd29d1f5f5effe7efe5f694e4ea734af
Ben Thomas <benthomas462@gmail.com>
2021-01-25 20:23:07 -0800

    Built Scafolding

--- src/React/Pages/Login/SunAndMoon.jsx
+++ src/React/Pages/Login/SunAndMoon.jsx
@@ -2,23 +2,23 @@ import react, {useState} from 'react';
 
 const SunAndMoon =() => {
 
-const [imageSrc, imageSrcUpdate] = useState ('/assets/img/moon.png');
+        const [imageSrc, imageSrcUpdate] = useState ('/assets/img/moon.png');
 
-const imageOver =( ) => {
-    imageSrcUpdate('/assets/img/sun.png');
-}
-const imageOut =( ) => {
-    imageSrcUpdate('/assets/img/moon.png');
-}
-    return (
-        <div>The Sun and The Moon
-            <img src={ imageSrc }
-            alt='The Sun and Moon'
-            onMouseOver={imageOver}
-            onMouseOut={imageOut}
-            />
-        </div>
-    )
+    const imageOver =( ) => {
+        imageSrcUpdate('/assets/img/sun.png');
+    }
+    const imageOut =( ) => {
+        imageSrcUpdate('/assets/img/moon.png');
+    }
+        return (
+            <div>The Sun and The Moon
+                <img src={ imageSrc }
+                alt='The Sun and Moon'
+                onMouseOver={imageOver}
+                onMouseOut={imageOut}
+                />
+            </div>
+        )
 }
 
 export default SunAndMoon
